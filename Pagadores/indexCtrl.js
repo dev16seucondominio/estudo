2e2cf3e46c59f45a23322f56387a04896f5bdb7c
Igor Santos <dev16.seucondominio@gmail.com>
2021-02-05 18:09:45 -0300

    No index eu apenas organizei o código e no top_bar_content eu arrumei alguns filtros, mas falta arrumar como ele está aparecendo

--- Pagadores/indexCtrl.js
+++ Pagadores/indexCtrl.js
@@ -12,7 +12,12 @@ pagadoresFinanc.controller("indexCtrl", [
       { id: 1, key: 'p_dois', nome: 'Luciana Antedeguemon', cpf: '000.000.000-01', nascimento: '10/10/2010', email: 'teste@seucondominio.com.br', telefone: '(62) 9 9674-5214', endereco: 'Rua Maria João, 115, Goiânia, Goiás-GO, 74590579' },
       { id: 2, key: 'p_tres', nome: 'Fernando Luiz', cpf: '000.000.000-01', nascimento: '10/10/2010', email: 'teste@seucondominio.com.br', telefone: '(62) 9 9674-5214', endereco: 'Rua Maria João, 115, Goiânia, Goiás-GO, 74590579' },
       { id: 3, key: 'p_quatro', nome: 'Michael Jackson', cpf: '000.000.000-01', nascimento: '10/10/2010', email: 'teste@seucondominio.com.br', telefone: '(62) 9 9674-5214', endereco: 'Rua Maria João, 115, Goiânia, Goiás-GO, 74590579' },
-      { id: 4, key: 'p_cinco', nome: 'Guilherme Lucas', cpf: '000.000.000-01', nascimento: '10/10/2010', email: 'teste@seucondominio.com.br', telefone: '(62) 9 9674-5214', endereco: 'Rua Maria João, 115, Goiânia, Goiás-GO, 74590579' }
+      { id: 4, key: 'p_cinco', nome: 'Ednaldo Pereira', cpf: '000.000.000-01', nascimento: '10/10/2010', email: 'teste@seucondominio.com.br', telefone: '(62) 9 9674-5214', endereco: 'Rua Maria João, 115, Goiânia, Goiás-GO, 74590579' },
+      { id: 5, key: 'p_seis', nome: 'José Augusto', cpf: '000.000.000-01', nascimento: '10/10/2010', email: 'teste@seucondominio.com.br', telefone: '(62) 9 9674-5214', endereco: 'Rua Maria João, 115, Goiânia, Goiás-GO, 74590579' },
+      { id: 6, key: 'p_sete', nome: 'Lucas Santos', cpf: '000.000.000-01', nascimento: '10/10/2010', email: 'teste@seucondominio.com.br', telefone: '(62) 9 9674-5214', endereco: 'Rua Maria João, 115, Goiânia, Goiás-GO, 74590579' },
+      { id: 7, key: 'p_oito', nome: 'Douglas Santos', cpf: '000.000.000-01', nascimento: '10/10/2010', email: 'teste@seucondominio.com.br', telefone: '(62) 9 9674-5214', endereco: 'Rua Maria João, 115, Goiânia, Goiás-GO, 74590579' },
+      { id: 8, key: 'p_nove', nome: 'Amanda Ribeiro', cpf: '000.000.000-01', nascimento: '10/10/2010', email: 'teste@seucondominio.com.br', telefone: '(62) 9 9674-5214', endereco: 'Rua Maria João, 115, Goiânia, Goiás-GO, 74590579' },
+      { id: 9, key: 'p_dez', nome: 'Leonardo Medeiros', cpf: '000.000.000-01', nascimento: '10/10/2010', email: 'teste@seucondominio.com.br', telefone: '(62) 9 9674-5214', endereco: 'Rua Maria João, 115, Goiânia, Goiás-GO, 74590579' }
     ]
 
     vm.filtro = [
@@ -28,57 +33,8 @@ pagadoresFinanc.controller("indexCtrl", [
       {opcao: 'Com emails'}
     ]
 
-    vm.scBorder = { color: 'red', position: '', type: 'solid', size: 'md' }
-
-    vm.badgeIcon = [
-      { icon: 'sc-icon-cadeado-aberto', color: 'green' },
-      { icon: 'sc-icon-carta-2', color: 'green' },
-      { icon: 'sc-icon-cafe', color: 'green' },
-      { icon: 'sc-icon-exclamacao-3', color: 'green' },
-      { icon: 'sc-icon-estrela', color: 'green' }
-    ]
-
-    vm.corIntensidade = ['-lighter', '-light', '', '-dark', '-darker'];
-
-    vm.cores = ['blue', 'cian', 'green', 'red', 'yellow', 'gray']
-
-    vm.listaBdg = [
-      { id: 0, key: 'bdg_um', color: 'green', intensidade: '-lighter'},
-      { id: 1, key: 'bdg_dois', color: 'green', intensidade: '-light'},
-      { id: 2, key: 'bdg_tres', color: 'green', intensidade: ''},
-      { id: 3, key: 'bdg_quatro', color: 'green', intensidade: '-dark'},
-      { id: 4, key: 'bdg_cinco', color: 'green', intensidade: '-darker'}
-    ]
-
-    vm.listaAlign = [
-      { id: 0, key: 'align_um', posicao: 'align-left', },
-      { id: 1, key: 'align_dois', posicao: 'align-center',},
-      { id: 2, key: 'align_tres', posicao: 'align-right',},
-      { id: 3, key: 'align_quatro', posicao: 'align-justify',}
-    ]
-
-    vm.listaCoresBg = ['red-lighter', 'red-light', 'red', 'red-dark', 'red-darker']
-
-    vm.scSides = ["", "-t", "-b", "-l", "-r", "-h", "-v"]
-
-    vm.changeColorAcc = function(cor) {
-      for(let i=0; i < vm.listaAcc.length; i++) {
-        vm.listaAcc[i].color = cor
-      }
-    }
-
-    vm.init = function() {
-
-    }
-
     vm.exibirForm = false
     vm.avancado = false
-
-    vm.changeColorBdg = function(cor) {
-      for(let i=0; i < vm.badgeIcon.length; i++) {
-        vm.badgeIcon[i].color = cor
-      }
-    }
 
     vm
 
