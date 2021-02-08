0088711ba17ab73493f0c3a1edf091f32f4b341c
Igor Santos <dev16.seucondominio@gmail.com>
2021-02-08 18:24:52 -0300

    Terminei a parte mais visual do formulário de cadastro, porém falta fazer o cadastro funcionar.

--- Pagadores/formCtrl.js
+++ Pagadores/formCtrl.js
@@ -47,6 +47,82 @@ pagadoresFinanc.controller("formularioCtrl", [
       {id: 3, key: 'banco_do_brasil', label: '001 - Banco do Brasil'}
     ]
 
+    vm.tipoConta = [
+      {id: 0, key: 'conta_corrente', label: 'Conta Corrente'},
+      {id: 1, key: 'poupanca', label: 'Poupança'},
+      {id: 2, key: 'conta_investimento', label: 'Conta Investimento'},
+      {id: 3, key: 'conta_investimento_2', label: 'Conta Investimento 2'},
+      {id: 4, key: 'conta_investimento_3', label: 'Conta Investimento 3'},
+      {id: 5, key: 'conta_investimento_4', label: 'Conta Investimento 4'},
+      {id: 6, key: 'conta_investimento_5', label: 'Conta Investimento 5'},
+      {id: 7, key: 'conta_investimento_6', label: 'Conta Investimento 6'},
+      {id: 8, key: 'fundo_de_reserva', label: 'Fundo de Reserva'},
+      {id: 9, key: 'quotas_de_capital', label: 'Quotas de Capital'},
+      {id: 10, key: 'ferias_decimo_terceiro', label: '13° Férias'},
+      {id: 11, key: 'fundo_de_eventos', label: 'Fundo de Eventos'},
+      {id: 12, key: 'fundo_de_obras', label: 'Fundo de Obras'},
+      {id: 13, key: 'f_reserva_compesa', label: 'Fundo de Reserva Compesa'}
+    ]
+
+    vm.correcao = [
+      {id: 0, key: 'inpc_ibge', label: 'INPC (IBGE)'},
+      {id: 1, key: 'igp_di_fgv', label: 'IGP-DI (FGV) - BR'},
+      {id: 2, key: 'ipa_m_fgv', label: 'IPA-M (FGV) - BR'},
+      {id: 3, key: 'ipa_di_fgv', label: 'IPA-DI (FGV) - BR'},
+      {id: 4, key: 'incc_m_fgv', label: 'INCC-M (FGV) - BR'},
+      {id: 5, key: 'incc_di_fgv', label: 'INCC-DI (FGV) - BR'},
+      {id: 6, key: 'tj_mg_tjmg', label: 'TJ-MG (TJMG) - BR'},
+      {id: 7, key: 'poupanca_br', label: 'Poupança - BR'},
+      {id: 8, key: 'ipc_di_fgv', label: 'IPC-DI (FGV) - BR'},
+      {id: 9, key: 'salario_minimo', label: 'Salário Mínimo - BR'},
+      {id: 10, key: 'encoge_718n', label: 'ENCOGE (JEBR0718N) - BR'},
+      {id: 11, key: 'encoge_719n', label: 'ENCOG (JEBR0719N) - BR'},
+      {id: 12, key: 'encoge_620n', label: 'ENCOGE (JEBR0620N) - BR'},
+      {id: 13, key: 'tj_sp_tjsp', label: 'TJ-SP (TJSP) - BR'},
+      {id: 14, key: 'encoge_820n', label: 'ENCONGE (JEBR0820N) - BR'},
+      {id: 15, key: 'igp_m_fgv', label: 'IGP-M (FGV) - BR'}
+    ]
+
+    vm.periodos = [
+      {id: 0, key: 'dias', label: 'Dias', default: false},
+      {id: 1, key: 'meses', label: 'Meses', default: true},
+      {id: 2, key: 'Anos', label: 'Anos', default: true}
+    ]
+
+    vm.anexos = [
+      {id:0, key: 'fotos', label: 'Fotos'},
+      {id:1, key: 'documentos', label: 'Documentos'}
+    ]
+
+    vm.enderecos = [
+      {
+        id: 1,
+        principal: true,
+        titulo: 'Casa',
+        cep: '74370577',
+        cidade: 'Goiânia - GO - Brasil',
+        logradouro: 'Rua Aulina Luiz',
+        complemento: 'Q.7 L.1',
+        bairro: 'Setor Santa Rita',
+      },
+      {
+        id: 2,
+        principal: false,
+        titulo: 'Trabalho',
+        cep: '74370580',
+        cidade: 'Goiânia - GO - Brasil',
+        logradouro: 'Rua 84',
+        complemento: 'Sala 505',
+        bairro: 'Setor Sul',
+      }
+    ]
+
+    vm.add = function() {
+      vm.addItem = function() {
+        vm.enderecos.push(vm.addMe)
+      }
+    }
+
     vm
   }
 ])
