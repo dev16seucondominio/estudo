bd30ac0f0bf2e4731dd1dcb4b50dec76dc6ee41c
Igor Santos <dev16.seucondominio@gmail.com>
2021-02-10 18:18:41 -0300

    Fiquei travado por causa de alguns erros, que ainda não solucionei, por isso hoje eu só trabalhei em cima do botão de salvar.

--- Pagadores/indexCtrl.js
+++ Pagadores/indexCtrl.js
@@ -1,57 +1,107 @@
 pagadoresFinanc = angular.module("pagadoresFinanc", ["sc.app.helpers"])
-pagadoresFinanc.run([
-  '$rootScope', function($rootScope) {
-  }
-])
-pagadoresFinanc.controller("indexCtrl", [
-  "$scope", function(s) {
-    vm = this
 
-    vm.listaPessoas = [
-      { id: 0, key: 'p_um', nome: 'Igor Santos', cpf: '000.000.000-01', nascimento: '10/10/2010', email: 'teste@seucondominio.com.br', telefone: '(62) 9 9674-5214', endereco: ''},
-      { id: 1, key: 'p_dois', nome: 'Luciana Antedeguemon', cpf: '000.000.000-01', nascimento: '10/10/2010', email: 'teste@seucondominio.com.br', telefone: '(62) 9 9674-5214', endereco: 'Rua Maria João, 115, Goiânia, Goiás-GO, 74590579' },
-      { id: 2, key: 'p_tres', nome: 'Fernando Luiz', cpf: '000.000.000-01', nascimento: '10/10/2010', email: 'teste@seucondominio.com.br', telefone: '(62) 9 9674-5214', endereco: 'Rua Maria João, 115, Goiânia, Goiás-GO, 74590579' },
-      { id: 3, key: 'p_quatro', nome: 'Michael Jackson', cpf: '000.000.000-01', nascimento: '10/10/2010', email: 'teste@seucondominio.com.br', telefone: '(62) 9 9674-5214', endereco: 'Rua Maria João, 115, Goiânia, Goiás-GO, 74590579' },
-      { id: 4, key: 'p_cinco', nome: 'Ednaldo Pereira', cpf: '000.000.000-01', nascimento: '10/10/2010', email: 'teste@seucondominio.com.br', telefone: '(62) 9 9674-5214', endereco: 'Rua Maria João, 115, Goiânia, Goiás-GO, 74590579' },
-      { id: 5, key: 'p_seis', nome: 'José Augusto', cpf: '000.000.000-01', nascimento: '10/10/2010', email: 'teste@seucondominio.com.br', telefone: '(62) 9 9674-5214', endereco: 'Rua Maria João, 115, Goiânia, Goiás-GO, 74590579' },
-      { id: 6, key: 'p_sete', nome: 'Lucas Santos', cpf: '000.000.000-01', nascimento: '10/10/2010', email: 'teste@seucondominio.com.br', telefone: '(62) 9 9674-5214', endereco: 'Rua Maria João, 115, Goiânia, Goiás-GO, 74590579' },
-      { id: 7, key: 'p_oito', nome: 'Douglas Santos', cpf: '000.000.000-01', nascimento: '10/10/2010', email: 'teste@seucondominio.com.br', telefone: '(62) 9 9674-5214', endereco: 'Rua Maria João, 115, Goiânia, Goiás-GO, 74590579' },
-      { id: 8, key: 'p_nove', nome: 'Amanda Ribeiro', cpf: '000.000.000-01', nascimento: '10/10/2010', email: 'teste@seucondominio.com.br', telefone: '(62) 9 9674-5214', endereco: 'Rua Maria João, 115, Goiânia, Goiás-GO, 74590579' },
-      { id: 9, key: 'p_dez', nome: 'Leonardo Medeiros', cpf: '000.000.000-01', nascimento: '10/10/2010', email: 'teste@seucondominio.com.br', telefone: '(62) 9 9674-5214', endereco: 'Rua Maria João, 115, Goiânia, Goiás-GO, 74590579' }
-    ]
+pagadoresFinanc.controller("PessoasIndexCtrl",
+  function() {
+    vmIdx = this
 
-    vm.filtro = [
-      {opcao: 'Com endereço'},
-      {opcao: 'Sem endereço'},
-      {opcao: 'Endereço completo'},
-      {opcao: 'Endereço Incompleto'},
-      {opcao: 'Sem CPF/CNPJ'},
-      {opcao: 'Com CPF/CNPJ'},
-      {opcao: 'Sem bloqueio inadimplente'},
-      {opcao: 'Com bloqueio inadimplente'},
-      {opcao: 'Sem emails'},
-      {opcao: 'Com emails'}
-    ]
-
-    vm.buscar = {
-      pesquisa: '',
-      listar: '',
-      filtrar: function() {
-        this.listar = this.pesquisa
+    vmIdx.listaPessoas = [
+      { id: 0, tipo: 'Pagador', sexo: 'm', deficiente: false,
+        nome: 'Igor Santos', cpf: '000.000.000-01', nascimento: '10/10/2010',
+        email: 'teste@seucondominio.com.br', telefone: '(62) 9 9674-5214',
+        endereco: [
+          { id: 123, principal: true, titulo: 'Casa', cep: '74370577', cidade: 'Goiânia',
+            logradouro: 'Rua Maria Luiz', completo: 'Quadra 7, Lote 10', bairro: 'Setor Central'
+          },
+          { id: 312, principal: false, titulo: 'Trabaio', cep: '740000', cidade: 'Goiânia',
+            logradouro: 'Rua 84', completo: 'Centro Comercial ANtonio JOão Sebba', bairro: 'Setor Sul'
+          }
+        ]
       },
-      nome: '',
-      documento: '',
-      telefone: '',
-      email: ''
-      buscaAvancada: function() {
+      { id: 1, tipo: 'Pagador', sexo: 'f', deficiente: false, nome: 'Luciana Pereira', cpf: '000.000.000-01', nascimento: '10/10/2010', email: 'igor@seucondominio.com.br', telefone: '(62) 9 9674-5214', endereco: vmIdx.listaEnderecos },
+      { id: 2, tipo: 'Pagador', sexo: 'm', deficiente: false, nome: 'Fernando Luiz', cpf: '000.000.000-01', nascimento: '10/10/2010', email: 'teste@seucondominio.com.br', telefone: '(62) 9 9674-5214', endereco: vmIdx.listaEnderecos},
+      { id: 3, tipo: 'Pagador', sexo: 'm', deficiente: false, nome: 'Michael Jackson', cpf: '000.000.000-01', nascimento: '10/10/2010', email: 'teste@seucondominio.com.br', telefone: '(62) 9 9674-5214', endereco: vmIdx.listaEnderecos}
+    ]
 
+    vmIdx.OpcoesAvanc = [
+      {label: 'Com endereço'},
+      {label: 'Sem endereço'},
+      {label: 'Endereço completo'},
+      {label: 'Endereço Incompleto'},
+      {label: 'Sem CPF/CNPJ'},
+      {label: 'Com CPF/CNPJ'},
+      {label: 'Sem bloqueio inadimplente'},
+      {label: 'Com bloqueio inadimplente'},
+      {label: 'Sem emails'},
+      {label: 'Com emails'}
+    ]
+
+    vmIdx.listar = {}
+
+    vmIdx.formCadastro = {
+      open: function() {
+        this.opened = true
+      },
+
+      close: function() {
+        this.opened = false
+      },
+
+      add: function() {
+        vmForm.params ||= {}
+
+        vmIdx.listaPessoas.unshift(
+          {
+            id: (vmIdx.listaPessoas.length) + 1,
+            nome: vmForm.params.nome,
+            cpf: vmForm.params.cpf,
+            nascimento: vmForm.params.nascimento,
+            email: vmForm.params.email,
+            telefone: vmForm.params.telefone
+          }
+        )
+
+        vmIdx.formCadastro.close()
       }
     }
 
-    vm.exibirForm = false
-    vm.avancado = false
+    // settings = {
+    //   contas: [
+    //   opcoes: [
+    //   ]
+    // }
 
-    vm
+    // filtro = {
+    //   avancado: false,
+    //   params: []
+
+    //   opcoes: [
+    //   ]
+
+    //   open: {
+
+    //   }
+    //   close: {
+
+    //   }
+
+    //   exec: function{
+    //     adçlfkasdlfasçdlkfjasd
+    //     listCtrl.buscar()
+    //   }
+    // }
+
+    vmIdx.buscar = {
+      filtroSimples: function() {
+        vmIdx.listar = vmIdx.aplicarFiltro
+      },
+      filtroAvanc: function() {
+        vmIdx.listar = vmIdx.aplicarFiltroAvancado
+      }
+    }
+
+    vmIdx.avancado = false;
+
+    vmIdx
 
   }
-])
+)
