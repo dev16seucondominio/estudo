899d5d334306bd90bc269ca6a12d0fe3e5a43394
Igor Santos <dev16.seucondominio@gmail.com>
2021-02-11 18:26:55 -0300

    Ainda estou tentando fazer uma comunicação das informações que recebo no form e passar para a lista de pessoas no index.

--- Pagadores/js/indexCtrl.js
+++ Pagadores/js/indexCtrl.js
@@ -1,75 +1,150 @@
 pagadoresFinanc = angular.module("pagadoresFinanc", ["sc.app.helpers"])
-  pagadoresFinanc.run([
-    '$rootScope', function($rootScope) {
+
+// pagadoresFinanc.config([
+//   "ENV",
+//   "$locationProvider",
+//   "$routeProvider",
+//   "$controllerProvider",
+//   "$compileProvider",
+//   "$filterProvider",
+//   "$provide",
+//   "$animateProvider",
+//   '$sceDelegateProvider',
+//   "$httpProvider",
+//   '$rootScopeProvider',
+//   function(ENV, $locationProvider, $routeProvider, $controllerProvider, $compileProvider, $filterProvider, $provide, $animateProvider, $sceDelegateProvider, $httpProvider, $rootScopeProvider) {
+
+//     app.lazy = {
+//       controller: $controllerProvider.register,
+//       directive: $compileProvider.directive,
+//       filter: $filterProvider.register,
+//       factory: $provide.factory,
+//       service: $provide.service,
+//       animation: $animateProvider.register,
+//     }
+//   }
+// ]);
+
+pagadoresFinanc.controller("PessoasIndexCtrl", ["formFactory",
+  function(formFactory) {
+    vmIdx = this
+
+    vmIdx.formFactory = undefined
+
+    vmIdx.init = function(){
+      console.log('ioasjdjias')
+      vmIdx.formFactory = new formFactory()
     }
-  ])
-  pagadoresFinanc.controller("pessoasCtrl", [
-    "$scope", function(s) {
 
-      s.listaPessoas = [
-        { id: 0, key: 'p_um', nome: 'Igor Santos', cpf: '000.000.000-01', telefone: '(62) 9 9674-5214', endereco: 'Rua Maria João, 115, Goiânia, Goiás-GO, 74590579'},
-        { id: 1, key: 'p_dois', nome: 'Luciana Antedeguemon', cpf: '000.000.000-01', telefone: '(62) 9 9674-5214', endereco: 'Rua Maria João, 115, Goiânia, Goiás-GO, 74590579' },
-        { id: 2, key: 'p_tres', nome: 'Fernando Luiz', cpf: '000.000.000-01', telefone: '(62) 9 9674-5214', endereco: 'Rua Maria João, 115, Goiânia, Goiás-GO, 74590579' },
-        { id: 3, key: 'p_quatro', nome: 'Michael Jackson', cpf: '000.000.000-01', telefone: '(62) 9 9674-5214', endereco: 'Rua Maria João, 115, Goiânia, Goiás-GO, 74590579' },
-        { id: 4, key: 'p_cinco', nome: 'Guilherme Lucas', cpf: '000.000.000-01', telefone: '(62) 9 9674-5214', endereco: 'Rua Maria João, 115, Goiânia, Goiás-GO, 74590579' }
-      ]
-
-    	s.listaAcc = [
-    		{ id: 0, key: 'acc_um', color: 'green', label_text: 'Accordion 1', intensidade: '-lighter'},
-    		{ id: 1, key: 'acc_dois', color: 'green',  label_text: 'Accordion 2', intensidade: '-light' },
-    		{ id: 2, key: 'acc_tres', color: 'green', label_text: 'Accordion 3', intensidade: '' },
-    		{ id: 3, key: 'acc_quatro', color: 'green', label_text: 'Accordion 4', intensidade: '-dark' },
-    		{ id: 4, key: 'acc_cinco', color: 'green', label_text: 'Accordion 5', intensidade: '-darker' }
-      ]
-
-      s.scBorder = { color: 'red', position: '', type: 'solid', size: 'md' }
-
-      s.badgeIcon = [
-        { icon: 'sc-icon-cadeado-aberto', color: 'green' },
-        { icon: 'sc-icon-carta-2', color: 'green' },
-        { icon: 'sc-icon-cafe', color: 'green' },
-        { icon: 'sc-icon-exclamacao-3', color: 'green' },
-        { icon: 'sc-icon-estrela', color: 'green' }
-      ]
-
-      s.corIntensidade = ['-lighter', '-light', '', '-dark', '-darker'];
-
-      s.cores = ['blue', 'cian', 'green', 'red', 'yellow', 'gray']
-
-      s.listaBdg = [
-        { id: 0, key: 'bdg_um', color: 'green', intensidade: '-lighter'},
-        { id: 1, key: 'bdg_dois', color: 'green', intensidade: '-light'},
-        { id: 2, key: 'bdg_tres', color: 'green', intensidade: ''},
-        { id: 3, key: 'bdg_quatro', color: 'green', intensidade: '-dark'},
-        { id: 4, key: 'bdg_cinco', color: 'green', intensidade: '-darker'}
-      ]
-
-    	s.listaAlign = [
-    		{ id: 0, key: 'align_um', posicao: 'align-left', },
-    		{ id: 1, key: 'align_dois', posicao: 'align-center',},
-    		{ id: 2, key: 'align_tres', posicao: 'align-right',},
-    		{ id: 3, key: 'align_quatro', posicao: 'align-justify',}
-    	]
-
-    	s.listaCoresBg = ['red-lighter', 'red-light', 'red', 'red-dark', 'red-darker']
-
-      s.scSides = ["", "-t", "-b", "-l", "-r", "-h", "-v"];
-
-      // s.changeSide = function() {
-
-      // }
-
-      s.changeColorAcc = function(cor) {
-        for(let i=0; i < s.listaAcc.length; i++) {
-          s.listaAcc[i].color = cor
-        }
+    vmIdx.listaPessoas = [
+      { id: 1, tipo: 'Pagador', sexo: 'm', deficiente: false, nome: 'Igor Santos',
+        cpf: '000.000.000-01', nascimento: '10/10/2010', email: 'teste@seucondominio.com.br',
+        telefone: '(62) 9 9674-5214',
+        enderecos: [
+          { id: 123, principal: true, titulo: 'Casa', cep: '74370577', cidade: 'Goiânia',
+            logradouro: 'Rua Maria Luiz', completo: 'Quadra 7, Lote 10', bairro: 'Setor Central'
+          },
+          { id: 312, principal: false, titulo: 'Trabaio', cep: '740000', cidade: 'Goiânia',
+            logradouro: 'Rua 84', completo: 'Centro Comercial ANtonio JOão Sebba', bairro: 'Setor Sul'
+          }
+        ]
+      },
+      { id: 2, tipo: 'Pagador', sexo: 'f', deficiente: false, nome: 'Luciana Pereira',
+        cpf: '000.000.000-01', nascimento: '10/10/2010', email: 'igor@seucondominio.com.br',
+        telefone: '(62) 9 9674-5214',
+        enderecos: [
+          { id: 987, principal: true, titulo: 'Casa', cep: '74370577', cidade: 'Goiânia',
+            logradouro: 'Rua Maria Luiz', completo: 'Quadra 7, Lote 10', bairro: 'Setor Central'
+          },
+          { id: 789, principal: false, titulo: 'Trabaio', cep: '740000', cidade: 'Goiânia',
+            logradouro: 'Rua 84', completo: 'Centro Comercial ANtonio JOão Sebba', bairro: 'Setor Sul'
+          }
+        ]
+      },
+      { id: 3, tipo: 'Pagador', sexo: 'm', deficiente: false, nome: 'Fernando Luiz',
+        cpf: '000.000.000-01', nascimento: '10/10/2010', email: 'teste@seucondominio.com.br',
+        telefone: '(62) 9 9674-5214',
+        enderecos: [
+          { id: 654, principal: true, titulo: 'Casa', cep: '74370577', cidade: 'Goiânia',
+            logradouro: 'Rua Maria Luiz', completo: 'Quadra 7, Lote 10', bairro: 'Setor Central'
+          },
+          { id: 456, principal: false, titulo: 'Trabaio', cep: '740000', cidade: 'Goiânia',
+            logradouro: 'Rua 84', completo: 'Centro Comercial ANtonio JOão Sebba', bairro: 'Setor Sul'
+          }
+        ]
+      },
+      { id: 4, tipo: 'Pagador', sexo: 'm', deficiente: false, nome: 'Michael Jackson',
+        cpf: '000.000.000-01', nascimento: '10/10/2010', email: 'teste@seucondominio.com.br',
+        telefone: '(62) 9 9674-5214',
+        enderecos: [
+          { id: 147, principal: true, titulo: 'Casa', cep: '74370577', cidade: 'Goiânia',
+            logradouro: 'Rua Maria Luiz', completo: 'Quadra 7, Lote 10', bairro: 'Setor Central'
+          },
+          { id: 741, principal: false, titulo: 'Trabaio', cep: '740000', cidade: 'Goiânia',
+            logradouro: 'Rua 84', completo: 'Centro Comercial ANtonio JOão Sebba', bairro: 'Setor Sul'
+          }
+        ]
       }
+    ]
 
-      s.changeColorBdg = function(cor) {
-        for(let i=0; i < s.badgeIcon.length; i++) {
-          s.badgeIcon[i].color = cor
-        }
+    vmIdx.OpcoesAvanc = [
+      {label: 'Com endereço'},
+      {label: 'Sem endereço'},
+      {label: 'Endereço completo'},
+      {label: 'Endereço Incompleto'},
+      {label: 'Sem CPF/CNPJ'},
+      {label: 'Com CPF/CNPJ'},
+      {label: 'Sem bloqueio inadimplente'},
+      {label: 'Com bloqueio inadimplente'},
+      {label: 'Sem emails'},
+      {label: 'Com emails'}
+    ]
+
+    vmIdx.listar = {}
+
+
+    // settings = {
+    //   contas: [
+    //   opcoes: [
+    //   ]
+    // }
+
+    // filtro = {
+    //   avancado: false,
+    //   params: []
+
+    //   opcoes: [
+    //   ]
+
+    //   open: {
+
+    //   }
+    //   close: {
+
+    //   }
+
+    //   exec: function{
+    //     listCtrl.buscar()
+    //   }
+    // }
+
+    // vmIdx.listaPessoas.push(vmIdx.formFactory.adicionarPessoa())
+
+    // vmIdx.listaPessoas = vmIdx.formFactory.adicionarPessoa(pessoa)
+
+
+    vmIdx.buscar = {
+      filtroSimples: function() {
+        vmIdx.listar = vmIdx.aplicarFiltro
+      },
+      filtroAvanc: function() {
+        vmIdx.listar = vmIdx.aplicarFiltroAvancado
       }
-
     }
-])
+
+    vmIdx.avancado = false;
+
+    return vmIdx
+
+  }
+]);
