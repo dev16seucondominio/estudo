c1d505bacbdc289aae5947c3d48dff05f10d50f0
Igor Santos <dev16.seucondominio@gmail.com>
2021-03-01 18:20:46 -0300

    Hoje fiz mais um pouco do módulo 03 do curso, o módulo está bem simples, bem capaz que amanhã eu já termine

--- modulo_03/crypto_wallet/app/controllers/welcome_controller.rb
+++ modulo_03/crypto_wallet/app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
   def index
+    cookies[:curso] = "Sistema de Informação [COOKIES]"
+    session[:curso] = "Sistema de Informação [SESSION]"
     @nome = params[:nome]
     @curso = params[:curso]
   end
