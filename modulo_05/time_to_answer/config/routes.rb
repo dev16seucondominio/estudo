838c0822597274c64307995f4cc07d85433729bf
Igor Santos <dev16.seucondominio@gmail.com>
2021-03-02 18:04:21 -0300

    Comecei a preparar um novo projeto, comecei com a instalação de algumas gems, configuei um pouco do I18n. Nesse projeto irei utilizar o devises para autenticação.

--- modulo_05/time_to_answer/config/routes.rb
+++ modulo_05/time_to_answer/config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  namespace :profiles_backoffice do
+    get 'welcome/index'
+  end
+  namespace :admins_backoffice do
+    get 'welcome/index'
+  end
+  devise_for :profiles
+  devise_for :admins
+  get 'welcome/index'
+  get 'inicio', to: 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'welcome#index'
 end
