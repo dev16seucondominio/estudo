bcf56c1d329225a49d35bbd7b87107b79d389395
Igor Santos <dev16.seucondominio@gmail.com>
2021-03-18 17:59:58 -0300

    Hoje vi muita coisa sobre autenticação, teve algumas partes que, mesmo seguindo o que foi feito no curso, deu erro. Eu continuei o curso, apenas vendo os vídeos, já que não consegui solucionar alguns erros e agora tô vendo sobre versionamento.

--- notebook-api/config/routes.rb
+++ notebook-api/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount_devise_token_auth_for 'User', at: 'auth'
   resources :kinds
 
   resources :contacts do
@@ -8,8 +9,11 @@ Rails.application.routes.draw do
     resource :phones, only: [:show]
     resource :phones, only: [:show], path: 'relationships/phones'
 
-    resource :address, only: [:show]
-    resource :address, only: [:show], path: 'relationships/address'
+    resource :phone, only: [:update, :create, :destroy]
+    resource :phone, only: [:update, :create, :destroy], path: 'relationships/phones'
+
+    resource :address, only: [:show, :update, :create, :destroy]
+    resource :address, only: [:show, :update, :create, :destroy], path: 'relationships/address'
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
